2bda125673c22735d431f3f39d2a7b0d0c78db80
eng-omar-ahmad-akll <eng.omar3akll@gmail.com>
2026-08-10 05:11:23 +0300

    OTP and pass forg and reset 3

--- config/sendEmail.js
+++ config/sendEmail.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        service: "Gmail",
+        service: "gmail",
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
